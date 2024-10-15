d810471901acd09567ffd17dc441162dbfc09b54
Gagan3221 <reanimatedgagan@gmail.com>
2024-10-15 22:58:47 +0530

    15-10-2024

---
+++ counter2.js
@@ -0,0 +1,29 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let current = init;
+    var increment = function(){
+        current++;
+        return current;
+    }
+    var decrement = function(){
+        current--;
+        return current;
+    }
+    var reset = function(){
+        current = init;
+        return current;
+    }
+
+    return {increment , decrement , reset}
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
