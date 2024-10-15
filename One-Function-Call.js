771741409ae8a506117ba5b79b99e9708799a6b4
Gagan3221 <reanimatedgagan@gmail.com>
2024-10-16 00:04:36 +0530

    JS 30 days

---
+++ One-Function-Call.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let result;
+    let hasBeenCalled = false;
+    return function(...args){
+        if(!hasBeenCalled){
+            result = fn(...args);
+            hasBeenCalled = true
+            return result;
+        }else{
+            return undefined;
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
