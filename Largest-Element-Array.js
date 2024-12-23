a4d4530d24bfac056b6c601777aafadca78dce5e
gagannn3211 <gagandeep.singh@gripinvest.in>
2024-12-23 17:44:53 +0530

    Largest element using brute force - sorting

---
+++ Largest-Element-Array.js
@@ -0,0 +1,11 @@
+
+function Maxele(arr){
+    arr.sort((a,b) => b-a)
+    return arr[0]
+}
+
+const arr1= [1,3,5,4,6,2];
+const arr2 = [3,2,6,9,10,99];
+
+console.log(Maxele(arr1))
+console.log(Maxele(arr2))
