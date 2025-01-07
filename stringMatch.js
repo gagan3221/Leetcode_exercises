3e376f45b0ce01140cc9b4a8903e4032a8d80c51
gagannn3211 <gagandeep.singh@gripinvest.in>
2025-01-07 22:56:51 +0530

    String Matching Array

---
+++ stringMatch.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var stringMatching = function(words) {
+    let result = [];
+    let n = words.length;
+    for(let i = 0 ; i < n ; i++){
+        for(j = 0 ; j < n ; j++){
+            if(i!==j && words[j].includes(words[i])){
+                result.push(words[i])
+                break;
+            }
+        }
+    }
+    return result;
+    
+};
